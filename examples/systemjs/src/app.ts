ac66fd3291321f8aaa4205e99b6fdcf781ce2e31
mariuszfoltak <mariusz.foltak@oracle.com>
2016-03-20 15:03:01 +0100

    Change examples

--- examples/systemjs/src/app.ts
+++ examples/systemjs/src/app.ts
@@ -2,21 +2,19 @@ import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {DatePipe} from "angular2/common";
 import {HTTP_PROVIDERS, Http} from "angular2/http";
-import {MfTable, MfDefaultSorter, MfDefaultPaginator} from 'mf-angular2-table/components';
+import {DataTableDirectives} from 'angular2-datatable/datatable';
 
 
 @Component({
     selector: 'app',
     templateUrl: 'src/app.html',
     providers: [HTTP_PROVIDERS],
-    directives: [MfTable, MfDefaultSorter, MfDefaultPaginator],
+    directives: [DataTableDirectives],
     pipes: [DatePipe]
 })
 export class App {
 
     private data = [];
-    private activePage = 1;
-    private rowsOnPage = 8;
 
     constructor(private http: Http) {
         http.get("/src/data.json")
