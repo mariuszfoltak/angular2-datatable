e90272e31b7f9cc260d83c06d5cd136eb13d0853
Tuupertunut <superemppu@live.fi>
2016-10-19 17:05:47 +0300

    Changed test configurations to more robust and maintainable ones. - These are made according to the Angular quickstart.

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,14 @@
-module.exports = function(config) {
+module.exports = function (config) {
+
+    var appBase = 'lib/';       // transpiled app JS and map files
+    var appSrcBase = 'src/';       // app source TS files
+    var appAssets = '/base/lib/'; // component assets fetched by Angular's compiler
+
     config.set({
 
         basePath: '.',
 
         frameworks: ['jasmine'],
-
-        files: [
-            // paths loaded by Karma
-            'node_modules/traceur/bin/traceur-runtime.js',
-            {pattern: 'node_modules/core-js/client/shim.js', included: true, watched: true},
-            {pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true},
-            {pattern: 'node_modules/reflect-metadata/Reflect.js', included: true, watched: true},
-            {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
-            {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
-            {pattern: 'node_modules/lodash/lodash.js', included: true, watched: true},
-            {pattern: 'karma-test-shim.js', included: true, watched: true},
-
-            // paths loaded via module imports
-            {pattern: 'lib/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/traceur/**/*.js', included: false, watched: true},
-
-            // paths to support debugging with source maps in dev tools
-            {pattern: 'src/**/*.ts', included: false, watched: false},
-            {pattern: 'lib/**/*.js.map', included: false, watched: false}
-        ],
-
-        // proxied base paths
-        proxies: {
-            // required for component assests fetched by Angular's compiler
-            '/src/': '/base/src/'
-        },
-
-        port: 9876,
-
-        logLevel: config.LOG_INFO,
-
-        colors: true,
-
-        autoWatch: true,
-
-        browsers: ['Chrome'],
 
         // Karma plugins loaded
         plugins: [
@@ -51,8 +18,110 @@ module.exports = function(config) {
             'karma-phantomjs-launcher'
         ],
 
-        // Coverage reporter generates the coverage
-        reporters: ['progress', 'dots', 'coverage'],
+        customLaunchers: {
+            // From the CLI. Not used here but interesting
+            // chrome setup for travis CI using chromium
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+
+        files: [
+            // System.js for module loading
+            'node_modules/systemjs/dist/system.src.js',
+
+            // Polyfills
+            'node_modules/core-js/client/shim.js',
+
+            // Zone.js
+            'node_modules/zone.js/dist/zone.js',
+            'node_modules/zone.js/dist/long-stack-trace-zone.js',
+            'node_modules/zone.js/dist/proxy.js',
+            'node_modules/zone.js/dist/sync-test.js',
+            'node_modules/zone.js/dist/jasmine-patch.js',
+            'node_modules/zone.js/dist/async-test.js',
+            'node_modules/zone.js/dist/fake-async-test.js',
+
+            // RxJs
+            {
+                pattern: 'node_modules/rxjs/**/*.js',
+                included: false,
+                watched: false
+            },
+            {
+                pattern: 'node_modules/rxjs/**/*.js.map',
+                included: false,
+                watched: false
+            },
+
+            // Angular 2
+            {
+                pattern: 'node_modules/@angular/**/*.js',
+                included: false,
+                watched: false
+            },
+            {
+                pattern: 'node_modules/@angular/**/*.js.map',
+                included: false,
+                watched: false
+            },
+
+            // Lodash
+            {
+                pattern: 'node_modules/lodash/lodash.js',
+                included: false,
+                watched: false
+            },
+
+            // The testing library
+            {
+                pattern: 'systemjs.config.js',
+                included: false,
+                watched: false
+            },
+            'karma-test-shim.js',
+
+            // transpiled application & spec code paths loaded via module imports
+            {
+                pattern: appBase + '**/*.js',
+                included: false,
+                watched: true
+            },
+
+            // asset (HTML & CSS) paths loaded via Angular's component compiler
+            // (these paths need to be rewritten, see proxies section)
+            {
+                pattern: appBase + '**/*.html',
+                included: false,
+                watched: true
+            },
+            {
+                pattern: appBase + '**/*.css',
+                included: false,
+                watched: true
+            },
+
+            // paths for debugging with source maps in dev tools
+            {
+                pattern: appSrcBase + '**/*.ts',
+                included: false,
+                watched: false
+            },
+            {
+                pattern: appBase + '**/*.js.map',
+                included: false,
+                watched: false
+            }
+        ],
+
+        // proxied base paths for loading assets
+        proxies: {
+            // required for component assets fetched by Angular's compiler
+            "/lib/": appAssets
+        },
+
+        exclude: [],
 
         // Source files that you wanna generate coverage for.
         // Do not include tests or libraries (these files will be instrumented by Istanbul)
@@ -60,11 +129,28 @@ module.exports = function(config) {
             'lib/**/!(*spec).js': ['coverage']
         },
 
+        // Coverage reporter generates the coverage
+        reporters: ['progress', 'dots', 'coverage'],
+
         coverageReporter: {
-            reporters:[
-                {type: 'json', subdir: '.', file: 'coverage-final.json'}
+            reporters: [
+                {
+                    type: 'json',
+                    subdir: '.',
+                    file: 'coverage-final.json'
+                }
             ]
         },
+
+        port: 9876,
+
+        colors: true,
+
+        logLevel: config.LOG_INFO,
+
+        autoWatch: true,
+
+        browsers: ['Chrome'],
 
         singleRun: true
     })
