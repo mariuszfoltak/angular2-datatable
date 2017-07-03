14ff64827cd3fa09cecc1f1d383504295da6ae81
Shalom Yerushalmy <syerushalmy@outbrain.com>
2017-07-03 07:45:12 +0300

    Added to lower case for easier search

--- examples/systemjs/app/data-filter.pipe.ts
+++ examples/systemjs/app/data-filter.pipe.ts
@@ -8,7 +8,7 @@ export class DataFilterPipe implements PipeTransform {
 
     transform(array: any[], query: string): any {
         if (query) {
-            return _.filter(array, row=>row.name.indexOf(query) > -1);
+          return _.filter(array, row => row.name.toLowerCase().indexOf(query.toLowerCase()) > -1);
         }
         return array;
     }
